a9f35b790b2b68afde1b489528bec90cc6f8b6dd
Evan Byrne <evantbyrne@gmail.com>
2016-11-01 23:29:26 -0600

    Volatile inventory implementation

--- src/main/java/com/evanbyrne/vending_machine_kata/inventory/InventoryProduct.java
+++ src/main/java/com/evanbyrne/vending_machine_kata/inventory/InventoryProduct.java
@@ -2,18 +2,21 @@ package com.evanbyrne.vending_machine_kata.inventory;
 
 public class InventoryProduct {
 
+    private final String name;
+
+    private final int stock;
+
     public InventoryProduct(final String name, final int stock) {
-        // TODO: Implement.
+        this.name = name;
+        this.stock = stock;
     }
 
     public String getName() {
-        // TODO: Implement.
-        return null;
+        return this.name;
     }
 
     public int getStock() {
-        // TODO: Implement.
-        return -1;
+        return this.stock;
     }
 
 }
--- src/main/java/com/evanbyrne/vending_machine_kata/inventory/VolatileInventoryService.java
+++ src/main/java/com/evanbyrne/vending_machine_kata/inventory/VolatileInventoryService.java
@@ -1,25 +1,43 @@
 package com.evanbyrne.vending_machine_kata.inventory;
 
 import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class VolatileInventoryService implements IInventoryService {
 
-    public InventoryProduct getProduct(final String key) {
-        // TODO: Implement.
-        return null;
+    private final TreeMap<String, InventoryProduct> inventory;
+
+    public VolatileInventoryService() {
+        this.inventory = new TreeMap<String, InventoryProduct>();
     }
-    
+
+    public InventoryProduct getProduct(final String key) {
+        return this.inventory.get(key);
+    }
+
     public void setProduct(final String key, final InventoryProduct inventoryProduct) {
-        // TODO: Implement.
+        this.inventory.put(key, inventoryProduct);
     }
 
     public SortedMap<String, InventoryProduct> getAllProducts() {
-        // TODO: Implement.
-        return null;
+        return this.inventory;
     }
 
     public InventoryProduct vendProduct(final String key) {
-        // TODO: Implement.
+        final InventoryProduct inventoryProduct = this.getProduct(key);
+
+        if(inventoryProduct != null) {
+            final int newStock = inventoryProduct.getStock() - 1;
+
+            if(newStock >= 0) {
+                final InventoryProduct newInventoryProduct = new InventoryProduct(inventoryProduct.getName(), newStock);
+                this.setProduct(key, newInventoryProduct);
+                return newInventoryProduct;
+            }
+
+            this.inventory.remove(key);
+        }
+
         return null;
     }
 
